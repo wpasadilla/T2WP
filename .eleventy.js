da7a1ff23ea6a82f768cb4f5e7649d6125778a28
Wilfredo Jr. Pasadilla <wilfredojp3@gmail.com>
2022-08-03 21:09:04 -0600

    Changed project from Gatsby to 11ty JAM stack based

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+
+  eleventyConfig.addWatchTarget("src/css/");
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
